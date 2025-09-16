261e94e24df0bf34b8f615bd357638eeb84faf16
Zhihuiwu646 <zhihui.wu@slu.edu>
2025-09-17 02:23:41 +0800

    ZhihuiWu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [file, setFile] = useState(null);
+  const fileSelect = (e) => {
+    const selectedFile = e.target.files && e.target.files[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+    }
+  };
+
   return (
-    <div style={{ padding: "20px", textAlign: "center" }}>
+    <div>
+      <div style={{ padding: "20px", textAlign: "center" }}>
       <h1>Welcome to Simulation Surgery</h1>
+      </div>
+      {file && (
+        <div>
+          <p style={{ padding: "20px" }}>Selected file: {file.name}
+          </p>
+        </div>
+      )}
+      <div style={{ position: "fixed", bottom: 20, display: "flex", alignItems: "center" }}>
+      </div>
+      <input id="fileInput"
+        type="file"
+        accept="image/*"
+        onChange={fileSelect}
+        style={{ display: "none" }}
+        />
+      <label htmlFor="fileInput" 
+        style={{ padding: "20px" }}> 
+      Select File
+      </label>
     </div>
   );
 }
