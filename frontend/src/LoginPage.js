47c8478754da8f1a586e0d5a0f6fe9fb842df7a6
Justin Duong <duongjustin287@gmail.com>
2025-09-22 15:51:03 -0500

    Resolved suggestions in pr

--- frontend/src/LoginPage.js
+++ frontend/src/LoginPage.js
@@ -16,7 +16,7 @@ function LoginPage() {
       const apiUrl = process.env.REACT_APP_API_URL || "http://127.0.0.1:5000";
       const res = await fetch(`${apiUrl}/api/login`, {
         method: "POST",
-        headers: {
+        headers: { 
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ username, password }),
