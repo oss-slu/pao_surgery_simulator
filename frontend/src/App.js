1a34f7c6ae125000b3f78623627679fd34b93c4c
Justin Duong <duongjustin287@gmail.com>
2025-09-29 14:11:25 -0500

    Implemented non-function prototype for VTK rendering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import LoginPage from "./LoginPage";
+import VTKViewer from "./VTKViewer";
 
 function App() {
   return (
     <div style={{ padding: "20px", textAlign: "center" }}>
       <h1>Welcome to Simulation Surgery</h1>
         <LoginPage />
+        <VTKViewer />
     </div>
   );
 }
