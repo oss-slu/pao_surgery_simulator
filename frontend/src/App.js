c30f5f59483d0603c45074bc69a1299e5bba98bf
Zhihuiwu646 <zhihui.wu@slu.edu>
2025-09-29 09:18:13 +0800

    Preview with limited pictures and changing the button position

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 
 function App() {
   const [file, setFile] = useState(null);
+  const [preview, setPreview] = useState(null);
   const fileSelect = (e) => {
     const selectedFile = e.target.files && e.target.files[0];
     if (selectedFile) {
       setFile(selectedFile);
+      setPreview(URL.createObjectURL(selectedFile));
     }
   };
 
@@ -15,23 +17,31 @@ function App() {
       <h1>Welcome to Simulation Surgery</h1>
       </div>
       {file && (
-        <div>
+        <div style={{ textAlign: "center" }}>
           <p style={{ padding: "20px" }}>Selected file: {file.name}
           </p>
         </div>
       )}
-      <div style={{ position: "fixed", bottom: 20, display: "flex", alignItems: "center" }}>
+      {preview && (
+        <div style={{ textAlign: "center", marginTop: "10px" }}>
+          <img
+            src={preview}
+            style={{ maxWidth: "800px" }}
+          />
+        </div>
+      )}
+      <div style={{ position: "fixed", bottom: 20, left: "50%", transform: "translateX(-50%)"}}>
+        <input id="fileInput"
+          type="file"
+          accept="image/*"
+          onChange={fileSelect}
+          style={{ display: "none" }}
+          />
+        <label htmlFor="fileInput" 
+          style={{ padding: "20px" }}> 
+        Select File
+        </label>
       </div>
-      <input id="fileInput"
-        type="file"
-        accept="image/*"
-        onChange={fileSelect}
-        style={{ display: "none" }}
-        />
-      <label htmlFor="fileInput" 
-        style={{ padding: "20px" }}> 
-      Select File
-      </label>
     </div>
   );
 }
