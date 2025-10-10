293bfa9e348b46225a93f716c138db3382587699
Zhihuiwu646 <zhihui.wu@slu.edu>
2025-10-10 09:18:30 +0800

    Setup database with PostgreSQL and the Flask API Endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,37 +1,86 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import os
+from config import config
+import psycopg2
 
 app = Flask(__name__)
 CORS(app)
 
-UPLOAD_FOLDER = "uploads"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+def get_connection():
+    params = config()
+    conn = psycopg2.connect(**params)
+    return conn
 
-@app.route("/api/hello", methods=["GET"])
-def hello():
-    return jsonify({"message": "Hello from Flask Backend!"})
+@app.route("/api/patients", methods=["POST"])
+def patients_add():
+    data = request.get_json()
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(
+        """INSERT INTO patients \
+        (patient_name, patient_age) VALUES (%s, %s)
+        RETURNING patient_id;""",
+        (data["patient_name"], data["patient_age"]))
+    new_id = cur.fetchone()[0]
+    conn.commit()
+    cur.close()
+    conn.close()
+    return jsonify({"message": "New Patient", "id": new_id})
 
-@app.route("/api/upload", methods=["POST"])
-def upload_file():
-    if "file" not in request.files:
-        return jsonify({"error": "No file part"}), 400
-    file = request.files["file"]
-    if file.filename == "":
-        return jsonify({"error": "No selected file"}), 400
-    filepath = os.path.join(UPLOAD_FOLDER, file.filename)
-    file.save(filepath)
-    return jsonify({"message": "File uploaded successfully", "filename": file.filename})
+@app.route("/api/patients", methods=["GET"])
+def patients_get():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT patient_id, patient_name, patient_age, date FROM patients;")
+    row = cur.fetchall()
+    cur.close()
+    conn.close()
+    patients = []
+    for attribute in row:
+        patients.append({
+            "patient_id": attribute[0],
+            "patient_name": attribute[1],
+            "patient_age": attribute[2],
+            "date": attribute[3].strftime("%Y-%m-%d %H:%M:%S")})
+    return jsonify(patients)
 
-@app.route("/api/files", methods=["GET"])
-def list_files():
-    files = os.listdir(UPLOAD_FOLDER)
-    return jsonify(files)
+@app.route("/api/images", methods=["POST"])
+def images_add():
+    data = request.get_json()
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(
+        """INSERT INTO images \
+        (patient_id, file_name, file_type) VALUES (%s, %s, %s)
+        RETURNING image_id""",
+        (data["patient_id"], data["file_name"], data["file_type"]))
+    new_id = cur.fetchone()[0]
+    conn.commit()
+    cur.close()
+    conn.close()
+    return jsonify({"message": "New Image", "id": new_id})
+
+@app.route("/api/images", methods=["GET"])
+def images_get():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT image_id, patient_id, file_name, file_type, upload_date FROM images;")
+    row = cur.fetchall()
+    cur.close()
+    conn.close()
+    images = []
+    for attribute in row:
+        images.append({
+            "image_id": attribute[0],
+            "patient_id": attribute[1],
+            "file_name": attribute[2],
+            "file_type": attribute[3],
+            "upload_date": attribute[4].strftime("%Y-%m-%d %H:%M:%S")})
+    return jsonify(images)
 
 @app.route("/", methods=["GET"])
 def home():
-    return jsonify({"message": "Backend is running!"})
-
+    return jsonify({"message": "Flask backend connected to PostgreSQL successfully!"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
