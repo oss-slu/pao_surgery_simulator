2bece7b6bd7dd29adc0eb61b60211d8bf76e733b
ssenecal1 <ssenecal1@gmail.com>
2026-09-08 18:44:31 -0500

    changed default username and password to be blank to match main branch

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -5,8 +5,8 @@ import { useNavigate } from "react-router-dom";
 
 function LoginPage({ apiBase, onLoginSuccess, onShowSignUp }) {
   const navigate = useNavigate();
-  const [username, setUsername] = useState("admin");
-  const [password, setPassword] = useState("admin");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
